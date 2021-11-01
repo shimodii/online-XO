5358aa87a0370b263c2f56069f1fb97b24d77414
mohsen <mohsen.minavand1384@gmail.com>
2021-11-01 15:34:05 +0330

    change print_board function

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ status=1 # set 0 for client and 1 for server (for turns)
 board=[[0,0,0],[0,0,0],[0,0,0]] # main board
 geo={1:(0,0),2:(0,1),3:(0,2),4:(1,0),5:(1,1),6:(1,2),7:(2,0),8:(2,1),9:(2,2)}
 
-def print_board():
-    for i in range(3):
-        print ("|",end="")
-        for j in range(3):
-            print (board[i][j],end="")
-            print ("|",end="")
-
-        print ("")
+def print_board() :
+    for x in range(len(board)) : 
+        for y in range(len(board[x])) : 
+            if y == 2 :
+                print(board[x][y] , end = "\n")
+                if x != 2 :
+                    print("----------")
+            else : 
+                print(board[x][y] , end = " | ")
 
 
 
