aa0428654c4f8b96403705f440bf7c0c9281a168
mohsen <mohsen.minavand1384@gmail.com>
2021-11-01 16:17:05 +0330

    create proccess class with send and get the board data

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-# pyngrok
+
+import socket
+import pickle 
+import pyngrok 
+
+
 status=1 # set 0 for client and 1 for server (for turns)
 board=[[0,0,0],[0,0,0],[0,0,0]] # main board
 geo={1:(0,0),2:(0,1),3:(0,2),4:(1,0),5:(1,1),6:(1,2),7:(2,0),8:(2,1),9:(2,2)}
@@ -17,22 +22,46 @@ def print_board() :
 
 
 
-def server():
-    while 1:
-        if status != 1:
-            pass
-
-        if status == 1:
-            turn=int(input("it's your turn!: "))
-
-
-def client(address):
-    while 1:
-        if status == 1:
-            pass
+class prcess : 
+    def __init__(self , connection_type , ip = None , port = None) :
+        self.server_config = ((ip , port)) 
         
-        if status != 1:
-            turn=int(input("it's your turn!: "))
+        if connection_type == "host" : 
+            self.server_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+            self.server_socket.bind(self.server_config)
+            print("server started . . . ") 
+            self.server_socket.listen(1)
+            self.client , self.client_info = self.server_socket.accept()
+            print(f"{self.client_info} has connected to the server")
+
+        elif connection_type == "client" : 
+            self.server_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+            self.server_socket.connect(self.server_config)
+
+    
+    def send_data_c(self , board_data) :
+        self.server_socket.send(board_data) 
+
+
+    def send_data_h(self , board_data) : 
+        self.client.send(board_data)
+
+
+
+    def get_data(self , type ) : 
+        if type == "host" : 
+            socket_connection = self.client
+
+        elif type == "client" :
+            socket_connection = self.server_socket
+
+        else : return "There is a problem for get type connection" # this return help is for prevent any error 
+        
+        board_data = socket_connection.recv(4096)
+        return board_data
+    
+        
+
 
 print ("hello welcome to XO game!")
 choice=input("""1- make a game!
@@ -40,8 +69,5 @@ choice=input("""1- make a game!
 
 print ("\n")
 
-if choice == '2':
-    addr=input("OK, enter friend game address: ")
-    client(addr)
     
 
