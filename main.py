ee1efdbf45b07243ebc37b74c2014d6a0a566385
am-shm <am.shahmoradi93@gmail.com>
2021-11-01 15:00:20 +0000

    main file updated

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+# pyngrok
+status=0
+board=[[0,0,0],[0,0,0],[0,0,0]]
+
+def print_board():
+    for i in range(3):
+        print ("|",end="")
+        for j in range(3):
+            print (board[i][j],end="")
+            print ("|",end="")
+
+        print ("")
+
+
+
+def server():
+    status=1
+
+
+def client():
+    status=0
